fef7c027e284eee06852099d922f78fca17c9fff
Yash-Raj-5424 <myash3499@gmail.com>
2026-09-12 00:49:18 +0530

    enableauditing on application class

--- src/main/java/com/yash/Scope/ScopeApplication.java
+++ src/main/java/com/yash/Scope/ScopeApplication.java
@@ -2,8 +2,10 @@ package com.yash.Scope;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class ScopeApplication {
 
 	public static void main(String[] args) {
